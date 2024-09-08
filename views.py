d6839bb8921527877eb1588f681ba613f60f7461
ushakovda <ushakov328@gmail.com>
2024-09-08 21:01:08 +0700

    Create views.py

---
+++ views.py
@@ -0,0 +1,9 @@
+def index():
+    with open('templates/index.html') as template:
+        return template.read()
+    
+def blog():
+    with open('templates/blog.html') as template:
+        return template.read()
+    
+    
